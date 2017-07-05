56e91ce8999596775757a2487c6a1b5043eb29c6
Kevin Kalis <kevin@swiftkind.com>
2017-07-05 22:38:21 +0800

    Add serving of media files

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,21 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='author')
+    category = models.ManyToManyField('blog.Category')
     heading = models.CharField(max_length=50)
     body = models.TextField()
-    cover = models.ImageField()
+    cover = models.ImageField(upload_to='covers')
 
     date_modified = models.DateTimeField(auto_now=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return "Post - {}".format(self.heading)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    icon = models.ImageField()
+    icon = models.ImageField(upload_to='icons')
+
+    def __str__(self):
+        return "Category - {}".format(self.name)
