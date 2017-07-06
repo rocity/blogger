6c2d3061dfa6e6bddcac0278dfdacf2d42b011cd
Kevin Kalis <kevin@swiftkind.com>
2017-07-06 19:50:50 +0800

    Add login backend

--- users/views.py
+++ users/views.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 
 from .forms import LoginForm
 
 
 def login(request):
     login_form = LoginForm()
+
     if request.method == 'POST':
-        pass
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/')
+            else:
+                login_form.add_error(None, 'There are no users that match your credentials.')
 
     context = {
         'login_form': login_form
